c4698c1d035bcffddccc865dd626c6c49fd2839d
ChristofferRysholt <eaacrch@students.eaaa.dk>
2019-06-01 12:49:05 +0200

    Headless CMS - Unika & Everyday

--- js/main.js
+++ js/main.js
@@ -201,14 +201,14 @@ fetch(sheetUrlTre)
     appendProduktEt(json.feed.entry);
   });
 
-  fetch(sheetUrlTre)
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(json) {
-      console.log(json);
-      appendProduktTo(json.feed.entry);
-    });
+fetch(sheetUrlTre)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(json) {
+    console.log(json);
+    appendProduktTo(json.feed.entry);
+  });
 
 function appendHeaderImage(headerImages) {
   console.log(headerImages);
@@ -276,6 +276,7 @@ function appendProduktEt(produkterEt) {
   }
   document.querySelector("#produktEt").innerHTML += htmlTemplate;
 }
+
 function appendProduktTo(produkterTo) {
   console.log(produkterTo);
   let htmlTemplate = "";
@@ -289,4 +290,444 @@ function appendProduktTo(produkterTo) {
       `;
   }
   document.querySelector("#produktTo").innerHTML += htmlTemplate;
+}
+
+"use strict";
+
+let sheetIdFire = "1ZH1V7zZcbu_jMtnCVHGFnQQWmUBT35u_C4XFoydxn3Y";
+let sheetNumberFire = 4;
+let sheetUrlFire = "https://spreadsheets.google.com/feeds/list/" + sheetIdFire + "/" + sheetNumberFire + "/public/full?alt=json";
+console.log(sheetUrlFire);
+
+fetch(sheetUrlFire)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(json) {
+    console.log(json);
+    appendTopLeft(json.feed.entry);
+  });
+
+fetch(sheetUrlFire)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(json) {
+    console.log(json);
+    appendTopCenter(json.feed.entry);
+  });
+
+fetch(sheetUrlFire)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(json) {
+    console.log(json);
+    appendTopRight(json.feed.entry);
+  });
+fetch(sheetUrlFire)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(json) {
+    console.log(json);
+    appendCenterLeft(json.feed.entry);
+  });
+fetch(sheetUrlFire)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(json) {
+    console.log(json);
+    appendCenterCenter(json.feed.entry);
+  });
+fetch(sheetUrlFire)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(json) {
+    console.log(json);
+    appendCenterRight(json.feed.entry);
+  });
+
+fetch(sheetUrlFire)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(json) {
+    console.log(json);
+    appendBotLeft(json.feed.entry);
+  });
+
+fetch(sheetUrlFire)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(json) {
+    console.log(json);
+    appendBotCenter(json.feed.entry);
+  });
+
+fetch(sheetUrlFire)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(json) {
+    console.log(json);
+    appendBotRight(json.feed.entry);
+  });
+
+function appendTopLeft(topLefts) {
+  console.log(topLefts);
+  let htmlTemplate = "";
+  for (let topLeft of topLefts) {
+    htmlTemplate += `
+          <section class="split-3" style="background:url('${topLeft['gsx$topleftbillede']['$t']}');background-size:cover;background-repeat: no-repeat;">
+            <section class="split-3-text">
+              <h1>${topLeft['gsx$topleft']['$t']}</h1>
+            </section>
+          </section>
+        `;
+  }
+  document.querySelector("#topLeft").innerHTML += htmlTemplate;
+}
+
+function appendTopCenter(topCenters) {
+  console.log(topCenters);
+  let htmlTemplate = "";
+  for (let topCenter of topCenters) {
+    htmlTemplate += `
+          <section class="split-3" style="background:url('${topCenter['gsx$topcenterbillede']['$t']}');background-size:cover;background-repeat: no-repeat;">
+            <section class="split-3-text">
+              <h1>${topCenter['gsx$topcenter']['$t']}</h1>
+            </section>
+          </section>
+        `;
+  }
+  document.querySelector("#topCenter").innerHTML += htmlTemplate;
+}
+
+function appendTopRight(topRights) {
+  console.log(topRights);
+  let htmlTemplate = "";
+  for (let topRight of topRights) {
+    htmlTemplate += `
+          <section class="split-3" style="background:url('${topRight['gsx$toprightbillede']['$t']}');background-size:cover;background-repeat: no-repeat;">
+            <section class="split-3-text">
+              <h1>${topRight['gsx$topright']['$t']}</h1>
+            </section>
+          </section>
+        `;
+  }
+  document.querySelector("#topRight").innerHTML += htmlTemplate;
+}
+
+function appendCenterLeft(centerLefts) {
+  console.log(centerLefts);
+  let htmlTemplate = "";
+  for (let centerLeft of centerLefts) {
+    htmlTemplate += `
+          <section class="split-3" style="background:url('${centerLeft['gsx$centerleftbillede']['$t']}');background-size:cover;background-repeat: no-repeat;">
+            <section class="split-3-text">
+              <h1>${centerLeft['gsx$centerleft']['$t']}</h1>
+            </section>
+          </section>
+        `;
+  }
+  document.querySelector("#centerLeft").innerHTML += htmlTemplate;
+}
+
+function appendCenterCenter(centerCenters) {
+  console.log(centerCenters);
+  let htmlTemplate = "";
+  for (let centerCenter of centerCenters) {
+    htmlTemplate += `
+          <section class="split-3" style="background:url('${centerCenter['gsx$centercenterbillede']['$t']}');background-size:cover;background-repeat: no-repeat;">
+            <section class="split-3-text">
+              <h1>${centerCenter['gsx$centercenter']['$t']}</h1>
+            </section>
+          </section>
+        `;
+  }
+  document.querySelector("#centerCenter").innerHTML += htmlTemplate;
+}
+
+function appendCenterRight(centerRights) {
+  console.log(centerRights);
+  let htmlTemplate = "";
+  for (let centerRight of centerRights) {
+    htmlTemplate += `
+          <section class="split-3" style="background:url('${centerRight['gsx$centerrightbillede']['$t']}');background-size:cover;background-repeat: no-repeat;">
+            <section class="split-3-text">
+              <h1>${centerRight['gsx$centerright']['$t']}</h1>
+            </section>
+          </section>
+        `;
+  }
+  document.querySelector("#centerRight").innerHTML += htmlTemplate;
+}
+
+function appendBotLeft(botLefts) {
+  console.log(botLefts);
+  let htmlTemplate = "";
+  for (let botLeft of botLefts) {
+    htmlTemplate += `
+          <section class="split-3" style="background:url('${botLeft['gsx$botleftbillede']['$t']}');background-size:cover;background-repeat: no-repeat;">
+            <section class="split-3-text">
+              <h1>${botLeft['gsx$botleft']['$t']}</h1>
+            </section>
+          </section>
+        `;
+  }
+  document.querySelector("#botLeft").innerHTML += htmlTemplate;
+}
+
+function appendBotCenter(botCenters) {
+  console.log(botCenters);
+  let htmlTemplate = "";
+  for (let botCenter of botCenters) {
+    htmlTemplate += `
+          <section class="split-3" style="background:url('${botCenter['gsx$botcenterbillede']['$t']}');background-size:cover;background-repeat: no-repeat;">
+            <section class="split-3-text">
+              <h1>${botCenter['gsx$botcenter']['$t']}</h1>
+            </section>
+          </section>
+        `;
+  }
+  document.querySelector("#botCenter").innerHTML += htmlTemplate;
+}
+
+function appendBotRight(botRights) {
+  console.log(botRights);
+  let htmlTemplate = "";
+  for (let botRight of botRights) {
+    htmlTemplate += `
+          <section class="split-3" style="background:url('${botRight['gsx$botrightbillede']['$t']}');background-size:cover;background-repeat: no-repeat;">
+            <section class="split-3-text">
+              <h1>${botRight['gsx$botright']['$t']}</h1>
+            </section>
+          </section>
+        `;
+  }
+  document.querySelector("#botRight").innerHTML += htmlTemplate;
+}
+
+"use strict";
+
+let sheetIdFem = "1ZH1V7zZcbu_jMtnCVHGFnQQWmUBT35u_C4XFoydxn3Y";
+let sheetNumberFem = 5;
+let sheetUrlFem = "https://spreadsheets.google.com/feeds/list/" + sheetIdFem + "/" + sheetNumberFem + "/public/full?alt=json";
+console.log(sheetUrlFem);
+
+fetch(sheetUrlFem)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(json) {
+    console.log(json);
+    appendTopLeftEveryday(json.feed.entry);
+  });
+
+fetch(sheetUrlFem)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(json) {
+    console.log(json);
+    appendTopCenterEveryday(json.feed.entry);
+  });
+
+fetch(sheetUrlFem)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(json) {
+    console.log(json);
+    appendTopRightEveryday(json.feed.entry);
+  });
+fetch(sheetUrlFem)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(json) {
+    console.log(json);
+    appendCenterLeftEveryday(json.feed.entry);
+  });
+fetch(sheetUrlFem)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(json) {
+    console.log(json);
+    appendCenterCenterEveryday(json.feed.entry);
+  });
+fetch(sheetUrlFem)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(json) {
+    console.log(json);
+    appendCenterRightEveryday(json.feed.entry);
+  });
+
+fetch(sheetUrlFem)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(json) {
+    console.log(json);
+    appendBotLeftEveryday(json.feed.entry);
+  });
+
+fetch(sheetUrlFem)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(json) {
+    console.log(json);
+    appendBotCenterEveryday(json.feed.entry);
+  });
+
+fetch(sheetUrlFem)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(json) {
+    console.log(json);
+    appendBotRightEveryday(json.feed.entry);
+  });
+
+function appendTopLeftEveryday(topLeftsEveryday) {
+  console.log(topLeftsEveryday);
+  let htmlTemplate = "";
+  for (let topLeftEveryday of topLeftsEveryday) {
+    htmlTemplate += `
+          <section class="split-3" style="background:url('${topLeftEveryday['gsx$toplefteverydaybillede']['$t']}');background-size:cover;background-repeat: no-repeat;">
+            <section class="split-3-text">
+              <h1>${topLeftEveryday['gsx$toplefteveryday']['$t']}</h1>
+            </section>
+          </section>
+        `;
+  }
+  document.querySelector("#topLeftEveryday").innerHTML += htmlTemplate;
+}
+
+function appendTopCenterEveryday(topCentersEveryday) {
+  console.log(topCentersEveryday);
+  let htmlTemplate = "";
+  for (let topCenterEveryday of topCentersEveryday) {
+    htmlTemplate += `
+          <section class="split-3" style="background:url('${topCenterEveryday['gsx$topcentereverydaybillede']['$t']}');background-size:cover;background-repeat: no-repeat;">
+            <section class="split-3-text">
+              <h1>${topCenterEveryday['gsx$topcentereveryday']['$t']}</h1>
+            </section>
+          </section>
+        `;
+  }
+  document.querySelector("#topCenterEveryday").innerHTML += htmlTemplate;
+}
+
+function appendTopRightEveryday(topRightsEveryday) {
+  console.log(topRightsEveryday);
+  let htmlTemplate = "";
+  for (let topRightEveryday of topRightsEveryday) {
+    htmlTemplate += `
+          <section class="split-3" style="background:url('${topRightEveryday['gsx$toprighteverydaybillede']['$t']}');background-size:cover;background-repeat: no-repeat;">
+            <section class="split-3-text">
+              <h1>${topRightEveryday['gsx$toprighteveryday']['$t']}</h1>
+            </section>
+          </section>
+        `;
+  }
+  document.querySelector("#topRightEveryday").innerHTML += htmlTemplate;
+}
+
+function appendCenterLeftEveryday(centerLeftsEveryday) {
+  console.log(centerLeftsEveryday);
+  let htmlTemplate = "";
+  for (let centerLeftEveryday of centerLeftsEveryday) {
+    htmlTemplate += `
+          <section class="split-3" style="background:url('${centerLeftEveryday['gsx$centerlefteverydaybillede']['$t']}');background-size:cover;background-repeat: no-repeat;">
+            <section class="split-3-text">
+              <h1>${centerLeftEveryday['gsx$centerlefteveryday']['$t']}</h1>
+            </section>
+          </section>
+        `;
+  }
+  document.querySelector("#centerLeftEveryday").innerHTML += htmlTemplate;
+}
+
+function appendCenterCenterEveryday(centerCentersEveryday) {
+  console.log(centerCentersEveryday);
+  let htmlTemplate = "";
+  for (let centerCenterEveryday of centerCentersEveryday) {
+    htmlTemplate += `
+          <section class="split-3" style="background:url('${centerCenterEveryday['gsx$centercentereverydaybillede']['$t']}');background-size:cover;background-repeat: no-repeat;">
+            <section class="split-3-text">
+              <h1>${centerCenterEveryday['gsx$centercentereveryday']['$t']}</h1>
+            </section>
+          </section>
+        `;
+  }
+  document.querySelector("#centerCenterEveryday").innerHTML += htmlTemplate;
+}
+
+function appendCenterRightEveryday(centerRightsEveryday) {
+  console.log(centerRightsEveryday);
+  let htmlTemplate = "";
+  for (let centerRightEveryday of centerRightsEveryday) {
+    htmlTemplate += `
+          <section class="split-3" style="background:url('${centerRightEveryday['gsx$centerrighteverydaybillede']['$t']}');background-size:cover;background-repeat: no-repeat;">
+            <section class="split-3-text">
+              <h1>${centerRightEveryday['gsx$centerrighteveryday']['$t']}</h1>
+            </section>
+          </section>
+        `;
+  }
+  document.querySelector("#centerRightEveryday").innerHTML += htmlTemplate;
+}
+
+function appendBotLeftEveryday(botLeftsEveryday) {
+  console.log(botLeftsEveryday);
+  let htmlTemplate = "";
+  for (let botLeftEveryday of botLeftsEveryday) {
+    htmlTemplate += `
+          <section class="split-3" style="background:url('${botLeftEveryday['gsx$botlefteverydaybillede']['$t']}');background-size:cover;background-repeat: no-repeat;">
+            <section class="split-3-text">
+              <h1>${botLeftEveryday['gsx$botlefteveryday']['$t']}</h1>
+            </section>
+          </section>
+        `;
+  }
+  document.querySelector("#botLeftEveryday").innerHTML += htmlTemplate;
+}
+
+function appendBotCenterEveryday(botCentersEveryday) {
+  console.log(botCentersEveryday);
+  let htmlTemplate = "";
+  for (let botCenterEveryday of botCentersEveryday) {
+    htmlTemplate += `
+          <section class="split-3" style="background:url('${botCenterEveryday['gsx$botcentereverydaybillede']['$t']}');background-size:cover;background-repeat: no-repeat;">
+            <section class="split-3-text">
+              <h1>${botCenterEveryday['gsx$botcentereveryday']['$t']}</h1>
+            </section>
+          </section>
+        `;
+  }
+  document.querySelector("#botCenterEveryday").innerHTML += htmlTemplate;
+}
+
+function appendBotRightEveryday(botRightsEveryday) {
+  console.log(botRightsEveryday);
+  let htmlTemplate = "";
+  for (let botRightEveryday of botRightsEveryday) {
+    htmlTemplate += `
+          <section class="split-3" style="background:url('${botRightEveryday['gsx$botrighteverydaybillede']['$t']}');background-size:cover;background-repeat: no-repeat;">
+            <section class="split-3-text">
+              <h1>${botRightEveryday['gsx$botrighteveryday']['$t']}</h1>
+            </section>
+          </section>
+        `;
+  }
+  document.querySelector("#botRightEveryday").innerHTML += htmlTemplate;
 }
