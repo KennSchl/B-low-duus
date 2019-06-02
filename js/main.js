8423d92184761daf9f6e215386fabfb14d857e0e
ChristofferRysholt <eaacrch@students.eaaa.dk>
2019-06-02 11:29:03 +0200

    headless CMS - Om_os

--- js/main.js
+++ js/main.js
@@ -731,3 +731,40 @@ function appendBotRightEveryday(botRightsEveryday) {
   }
   document.querySelector("#botRightEveryday").innerHTML += htmlTemplate;
 }
+
+"use strict";
+
+let sheetIdSeks = "1ZH1V7zZcbu_jMtnCVHGFnQQWmUBT35u_C4XFoydxn3Y";
+let sheetNumberSeks = 6;
+let sheetUrlSeks = "https://spreadsheets.google.com/feeds/list/" + sheetIdSeks + "/" + sheetNumberSeks + "/public/full?alt=json";
+console.log(sheetUrlSeks);
+
+fetch(sheetUrlSeks)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(json) {
+    console.log(json);
+    appendOmOsImage(json.feed.entry);
+  });
+
+  function appendOmOsImage(omOsImages) {
+    console.log(omOsImages);
+    let htmlTemplate = "";
+    for (let omOsImage of omOsImages) {
+      htmlTemplate += `
+          <section class="om-os-wide" style="background:url('${omOsImage['gsx$omosbillede']['$t']}');background-size:cover;background-repeat: no-repeat;">
+          </section>
+          <section class="om-os-text">
+            <h2>${omOsImage['gsx$omostitelet']['$t']}</h2>
+            <p>${omOsImage['gsx$omostextet']['$t']}</p>
+            <p>${omOsImage['gsx$omostextto']['$t']}</p>
+            <h2>${omOsImage['gsx$omostitelto']['$t']}</h2>
+            <p>${omOsImage['gsx$omostexttre']['$t']}</p>
+            <p>${omOsImage['gsx$omostextfire']['$t']}</p>
+          </section>
+
+        `;
+    }
+    document.querySelector("#omOsBillede").innerHTML += htmlTemplate;
+  }
